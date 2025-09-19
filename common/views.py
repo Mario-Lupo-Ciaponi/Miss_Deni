6f68f1723c1ce3fe87d71f30635ac22673657e8b
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2025-09-19 22:33:45 +0300

    Feat: added header

---
+++ common/views.py
@@ -0,0 +1,8 @@
+from http.client import HTTPResponse
+
+from django.http import HttpRequest
+from django.shortcuts import render
+
+
+def index(request: HttpRequest) -> HTTPResponse:
+    return render(request, "common/../templates/index.html")
