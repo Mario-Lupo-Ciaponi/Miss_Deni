e71c70d94d5e16a9249693bf70be4b28379c54c4
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2025-10-12 14:00:10 +0300

    Feat: finihed register page.

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,4 +11,31 @@ class UserRegistrationForm(UserCreationForm):
         fields = [
             "username",
             "email",
+            "password1",
+            "password2",
         ]
+        labels = {
+            "username": "Потребителско име",
+            "email": "Имейл адрес",
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password1"].label = "Парола"
+        self.fields["password2"].label = "Потвърждаване на парола"
+
+        self.fields["username"].widget.attrs.update({
+            "placeholder": "Напишете потрбителско Ви име"
+        })
+
+        self.fields["email"].widget.attrs.update({
+            "placeholder": "Нашете имейл адреса Ви"
+        })
+
+        self.fields["password1"].widget.attrs.update({
+            "placeholder": "Напишете парола"
+        })
+
+        self.fields["password2"].widget.attrs.update({
+            "placeholder": "Напишете отново паролата"
+        })
